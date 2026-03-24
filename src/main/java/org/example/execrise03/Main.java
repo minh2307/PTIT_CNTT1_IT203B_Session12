8fa3a367e260c6e0183c8f3e49017d4dea7bbe65
Nguyễn Sơn Minh <minh23923@gmail.com>
2026-03-24 15:26:07 +0700

    Update Main.java

--- src/main/java/org/example/execrise03/Main.java
+++ src/main/java/org/example/execrise03/Main.java
@@ -1,6 +1,6 @@
 package org.example.execrise03;
 import java.math.BigDecimal;
-
+// JDBC bắt buộc phải gọi registerOutParameter() trước khi thực thi vì với CallableStatement, driver cần biết trước tham số nào là OUT và kiểu dữ liệu của nó để chuẩn bị vùng nhận kết quả từ Stored Procedure.
 public class Main {
     public static void main(String[] args) {
         SurgeryFeeService service = new SurgeryFeeService();
