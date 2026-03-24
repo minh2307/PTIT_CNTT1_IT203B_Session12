64a2d89bee437bb626e1c6d1b85e4818385f860f
Nguyễn Sơn Minh <minh23923@gmail.com>
2026-03-24 15:25:51 +0700

    Update Main.java

--- src/main/java/org/example/execrise02/Main.java
+++ src/main/java/org/example/execrise02/Main.java
@@ -2,6 +2,8 @@ package org.example.execrise02;
 
 import java.util.Scanner;
 
+//Vì setDouble(), setInt() của PreparedStatement không truyền dữ liệu kiểu chuỗi theo định dạng do hệ điều hành hay người dùng quyết định, mà truyền giá trị đúng kiểu dữ liệu Java xuống JDBC Driver để driver tự chuyển sang định dạng mà DB hiểu.
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
