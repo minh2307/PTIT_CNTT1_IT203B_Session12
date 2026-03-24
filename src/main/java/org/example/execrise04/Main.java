e305c02e461b34b63543efe805591a4bc38f4c86
Nguyễn Sơn Minh <minh23923@gmail.com>
2026-03-24 15:26:37 +0700

    Update Main.java

--- src/main/java/org/example/execrise04/Main.java
+++ src/main/java/org/example/execrise04/Main.java
@@ -3,6 +3,7 @@ package org.example.execrise04;
 import java.util.ArrayList;
 import java.util.List;
 
+// Khi Database Server phải Parse và tạo Execution Plan 1.000 lần cho cùng một cấu trúc câu lệnh, đó là một sự lãng phí tài nguyên rất lớn vì server cứ phải lặp lại cùng một công việc mà lẽ ra chỉ cần làm một lần.
 public class Main {
     public static void main(String[] args) {
         List<TestResult> list = new ArrayList<>();
